2e220cf729626649b7f469c0efafa25b9dddb871
Travis Fischer <fisch0920@gmail.com>
2020-05-02 02:26:15 -0400

    Updates

---
+++ precache-manifest.17f168c7895844d16bbfea4bf09f8eee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "163fb01099fcb5dc860883bf4e1e74c4",
+    "url": "/react-blobby-blob/index.html"
+  },
+  {
+    "revision": "ddcba40928844b181afb",
+    "url": "/react-blobby-blob/static/css/main.5757f3bb.chunk.css"
+  },
+  {
+    "revision": "f87f1c2865c76a4ccf7d",
+    "url": "/react-blobby-blob/static/js/2.87093b3c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-blobby-blob/static/js/2.87093b3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddcba40928844b181afb",
+    "url": "/react-blobby-blob/static/js/main.294189c4.chunk.js"
+  },
+  {
+    "revision": "bbcc159ca7c2a1abaf7a",
+    "url": "/react-blobby-blob/static/js/runtime-main.5c3ed832.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-blobby-blob/precache-manifest.17f168c7895844d16bbfea4bf09f8eee.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-blobby-blob/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
