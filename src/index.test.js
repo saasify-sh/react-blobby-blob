6cf9f74b63862ec873d280d75db18c428e110076
Travis Fischer <fisch0920@gmail.com>
2020-05-03 14:30:41 -0400

    👴

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,7 @@
-import { ExampleComponent } from '.'
+import { Blob } from '.'
 
 describe('ExampleComponent', () => {
   it('is truthy', () => {
-    expect(ExampleComponent).toBeTruthy()
+    expect(Blob).toBeTruthy()
   })
 })
